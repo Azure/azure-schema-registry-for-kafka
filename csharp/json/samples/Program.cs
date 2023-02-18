a0f42c8ee6919c16c8183f9f0e829c9fef0385eb
hmlam@microsoft.com <hmlam@microsoft.com>
2023-02-17 18:02:27 -0800

    Json .Net sample with just Newtonsoft.Json dependency

---
+++ csharp/json/samples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace EventHubsForKafkaSample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string brokerList = ConfigurationManager.AppSettings["EH_FQDN"];
+            string connectionString = ConfigurationManager.AppSettings["EH_JAAS_CONFIG"];
+            string topic = ConfigurationManager.AppSettings["EH_NAME"];
+            string consumerGroup = ConfigurationManager.AppSettings["KAFKA_GROUP"];
+
+            Console.WriteLine("Initializing Producer");
+            Worker.Producer(brokerList, connectionString, topic).Wait();
+            Console.WriteLine();
+            Console.WriteLine("Initializing Consumer");
+            Worker.Consumer(brokerList, connectionString, consumerGroup, topic);
+            Console.ReadKey();
+        }
+    }
+}
