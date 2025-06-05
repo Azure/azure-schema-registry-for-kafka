735b3f3bf6c2fcd24ae5ace8d6be88a3c2a613a3
Nick Hardwick <nickhardwick@microsoft.com>
2025-06-05 10:17:33 -0700

    Add Json serializer test project

---
+++ csharp/json/tests/KafkaJsonSerializerTests.cs
@@ -0,0 +1,16 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Kafka.SchemaRegistry.Json.Tests
+{
+	[TestClass]
+	public sealed class KafkaJsonSerializerTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(true, true, "Test setup");
+		}
+	}
+}
